ab22c5dd220eee8eae2e4c23161f7e7264ad149f
rajistic <75992491+rajistic@users.noreply.github.com>
2020-12-24 13:46:45 +0530

    Create Asynchronous.js

---
+++ src/CommonComponents/Asynchronous.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Asynchronous.css'
+
+function Asynchronous() {
+  return (
+    <div>
+    <select class="ui dropdown selectcity"  style={{height:43}} >
+    
+    <option value="">Select city</option>
+    <br/>
+    <option value="1">ghaziabad</option>
+    <br/>
+    <option value="0">Noida</option>
+  </select>
+    </div>
+  )
+}
+
+export default Asynchronous
